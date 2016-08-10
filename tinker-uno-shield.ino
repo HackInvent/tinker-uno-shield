cec73ff3f7cab92d8a37c54990cd3e8a5f6520c6
HackInvent <HackInvent@users.noreply.github.com>
2016-08-10 17:52:29 +0200

    Update tinker-uno-shield.ino

--- tinker-uno-shield.ino
+++ tinker-uno-shield.ino
@@ -3,10 +3,11 @@
 #define TINKER_UNO_SHIELD   //on utilise le module TINKER_UNO_SHIELD
 #define TINKER_BUTTON       //on utilise le module TINKER_BUTTON
 
-setup(){
 
-}
+TinkerLED maLed(D3); //définition de la LED sur le port D3 du TINKER UNO SHIELD
 
 loop(){
-
+  maLed.on();        //allumer la LED
+  delay(1000);       //retarder le programme 1sec
+  maLed.off();       //eteindre la LED
 }
